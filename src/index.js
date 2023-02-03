20260bc1cbc57d8de6aef2e46f4e88fb05e815c3
ARTEM3221 <goroholinsyartem@gmail.com>
2023-02-04 01:16:43 +0200

    5

--- src/index.js
+++ src/index.js
@@ -1,45 +1,62 @@
+import { createApp } from 'vue'
 import axios from 'axios'
 
-new Vue({
-  el: '#app',
-  data() {
-    return {
-      students: [],
-      newStudent: {
-        name: '',
-        group: '',
-        mark: '',
-        isDonePr: false
-      },
-      editingStudent: null
-    }
-  },
-  mounted() {
-    this.getStudents()
-  },
-  methods: {
-    async getStudents() {
-      const response = await axios.get('http://34.82.81.113:3000/students')
-      this.students = response.data
-    },
-    async deleteStudent(_id) {
-      await axios.delete(`http://34.82.81.113:3000/students/${_id}`) 
-      this.getStudents() 
-    }, 
-    async addStudent() { 
-      const response = await axios.post('http://34.82.81.113:3000/students', this.newStudent) 
-      this.students.push(response.data) 
-      this.newStudent = { name: '', group: '', mark: '', isDonePr: false } 
-    }, 
-    async updateStudent() { await axios.put(`http://34.82.81.113:3000/students/${this.editingStudent._id}`, this.editingStudent)
-      this.getStudents()
-      this.editingStudent = null
-      },
-      editStudent(student) {
-      this.editingStudent = student
-      },
-      cancelEditing() {
-      this.editingStudent = null
-      }
-      }
-});
+const app = createApp({
+data() {
+return {
+searchTerm: '',
+students: [],
+newStudent: {
+name: '',
+group: '',
+mark: '',
+isDonePr: false
+},
+editingStudent: null
+}
+},
+mounted() {
+this.getStudents()
+},
+computed: {
+  filteredStudents() {
+    return this.students.filter(student => student.name.toLowerCase().includes(this.searchTerm.toLowerCase()))
+  }
+},
+methods: {
+getStudents() {
+axios.get('http://34.82.81.113:3000/students')
+.then(response => {
+this.students = response.data
+})
+},
+deleteStudent(_id) {
+axios.delete(`http://34.82.81.113:3000/students/${_id}`)
+.then(() => {
+this.getStudents()
+})
+},
+addStudent() {
+axios.post('http://34.82.81.113:3000/students', this.newStudent)
+.then(response => {
+this.students.push(response.data)
+this.newStudent = { name: '', group: '', mark: '', isDonePr: false }
+})
+},
+updateStudent() {
+axios.put(`http://34.82.81.113:3000/students/${this.editingStudent._id}`, this.editingStudent)
+.then(() => {
+this.getStudents()
+this.editingStudent = null
+})
+},
+editStudent(student) {
+this.editingStudent = student
+},
+cancelEditing() {
+this.editingStudent = null
+}
+}
+})
+
+app.mount('#app')
